986d6d27a9c3872d381f5299e48f551d8021ed4a
aiyk <aiyk.ekwe@gmail.com>
2019-03-05 21:27:14 +0100

    forget password section

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
+let sections = {
+    login: true,
+    forgetPass: false,
+    resetSuccess: false
+}
+
 let userData = {
     username: "aiyk",
     password: "pass123"
 }
+
 const submit = () => {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
     if (username != userData.username || password != userData.password) {
-        document.getElementById("err").style.visibility = "visible";
+        document.getElementById("err").style.display = "flex";
     } else {
-        document.getElementById("err").style.visibility = "hidden";
+        document.getElementById("err").style.display = "none";
     }
+}
+
+const resetPass = () => {
+    let email = document.getElementById("email").value;
+
+    if (!email) {
+        document.getElementById("errMsg").innerHTML = "Kindly supply your email";
+        document.getElementById("err").style.display = "flex";
+    } else {
+        document.getElementById("err").style.display = "none";
+    }
+}
+
+const forgotPass = () => {
+    document.getElementById("err").style.display = "none";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("resetFrm").style.display = "block";
+    document.getElementById("title").innerHTML = "Forgot your password?";
+}
+
+const login = () => {
+    document.getElementById("err").style.display = "none";
+    document.getElementById("login").style.display = "block";
+    document.getElementById("resetFrm").style.display = "none";
+    document.getElementById("title").innerHTML = "Login";
 }
